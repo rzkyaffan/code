23a7451e56d4176d357e15bb72bf1816abe2790a
affan <affandirrh@gmail.com>
2024-05-12 16:52:04 +0700

    menambahkan seluruh file untuk portofolio saya

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * toggle the navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+// Mendapatkan elemen di halaman HTML dengan id 'tahun'
+var tahunElemen = document.getElementById('year');
+
+// Membuat objek Date untuk mendapatkan tahun saat ini
+var tahunSaatIni = new Date().getFullYear();
+
+// Memperbarui teks konten elemen dengan tahun saat ini
+tahunElemen.textContent = '© ' + tahunSaatIni + ' afann.dev . All Rights Reserverd';
+
+
+/**
+ * back to top & header
+ */
+
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
